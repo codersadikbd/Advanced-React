cb2617dcb0495b41af1e94640a668974da771c0a
codersadikbd <devsadikbd@gmail.com>
2025-04-18 10:26:53 +0600

    refactor: simplify keystone configuration by removing unused authentication and session management code

--- finished-application/backend/keystone.ts
+++ finished-application/backend/keystone.ts
@@ -1,87 +1,24 @@
-import { createAuth } from "@keystone-next/auth";
-import { config, createSchema } from "@keystone-next/keystone/schema";
-import {
-  withItemData,
-  statelessSessions,
-} from "@keystone-next/keystone/session";
-import { permissionsList } from "./schemas/fields";
-import { Role } from "./schemas/Role";
-import { OrderItem } from "./schemas/OrderItem";
-import { Order } from "./schemas/Order";
-import { CartItem } from "./schemas/CartItem";
-import { ProductImage } from "./schemas/ProductImage";
-import { Product } from "./schemas/Product";
-import { User } from "./schemas/User";
-import "dotenv/config";
-import { insertSeedData } from "./seed-data";
-import { sendPasswordResetEmail } from "./lib/mail";
-import { extendGraphqlSchema } from "./mutations";
-
-function check(name: string) {}
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import 'dotenv/config';
 
 const databaseURL =
-  process.env.DATABASE_URL || "mongodb://localhost/keystone-sick-fits-tutorial";
-
-const sessionConfig = {
-  maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
-  secret: process.env.COOKIE_SECRET,
-};
-
-const { withAuth } = createAuth({
-  listKey: "User",
-  identityField: "email",
-  secretField: "password",
-  initFirstItem: {
-    fields: ["name", "email", "password"],
-    // TODO: Add in inital roles here
-  },
-  passwordResetLink: {
-    async sendToken(args) {
-      // send the email
-      await sendPasswordResetEmail(args.token, args.identity);
+  process.env.databaseURL ||
+  'mongodb+srv://sicksadik:Sadik@2000@sickclusersadik.yaxqrtu.mongodb.net/?retryWrites=true&w=majority&appName=SickCluserSadik';
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
     },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+  },
+  lists: createSchema({
+    // Schema items go here
+  }),
+  ui: {
+    isAccessAllowed: () => true,
   },
 });
-
-export default withAuth(
-  config({
-    // @ts-ignore
-    server: {
-      cors: {
-        origin: [process.env.FRONTEND_URL],
-        credentials: true,
-      },
-    },
-    db: {
-      adapter: "mongoose",
-      url: databaseURL,
-      async onConnect(keystone) {
-        console.log("Connected to the database!");
-        if (process.argv.includes("--seed-data")) {
-          await insertSeedData(keystone);
-        }
-      },
-    },
-    lists: createSchema({
-      // Schema items go in here
-      User,
-      Product,
-      ProductImage,
-      CartItem,
-      OrderItem,
-      Order,
-      Role,
-    }),
-    extendGraphqlSchema,
-    ui: {
-      // Show the UI only for poeple who pass this test
-      isAccessAllowed: ({ session }) =>
-        // console.log(session);
-        !!session?.data,
-    },
-    session: withItemData(statelessSessions(sessionConfig), {
-      // GraphQL Query
-      User: `id name email role { ${permissionsList.join(" ")} }`,
-    }),
-  })
-);
